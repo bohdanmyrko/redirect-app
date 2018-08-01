edc8560f9aa2a414b60305037b9dab449219f038
bohdanmyrko <41728864+bohdanmyrko@users.noreply.github.com>
2018-08-01 16:52:04 +0300

    return to normal stage

--- redirectapp/views.py
+++ redirectapp/views.py
@@ -7,11 +7,11 @@ import requests, json
 def prices(request):
     if request.method == 'POST':
         print(request.POST['url'])
-        #response = requests.get(
-         #   request.POST['url'], verify=False,
-          #  stream=True)
-#return HttpResponse(response.raw.read().decode('cp1251').encode('utf-8'))
-        return HttpResponse(json.dumps({space : response}))
+        response = requests.get(
+            request.POST['url'], verify=False,
+            stream=True)
+        return HttpResponse(response.raw.read().decode('cp1251').encode('utf-8'))
+        #return HttpResponse(json.dumps({space : response}))
 
 
 @csrf_exempt
