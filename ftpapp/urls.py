0349cd9f1533ace6807d68ce239bf280e16c44e9
Igor <igor.ievsieienko@ctdev.io>
2018-09-10 11:49:12 +0300

    Modifications made by Andrei to fetch files with prices from  Medservice ftp

--- ftpapp/urls.py
+++ ftpapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path
+from django.views.generic.base import View
+
 from . import views
 
 app_name = 'ftpapp'
 
 urlpatterns = [
     # re_path(r'connectftp/(?P<date>\d{4}-\d{2}-\d{2})$', views.connectftp, name = 'connectftp')
-    re_path(r'connectftp/fetchdata$', views.connectftp, name = 'connectftp'),
-    re_path(r'byname$',views.filebyname,name = 'byname'),
+    re_path(r'connectftp/fetchdata$', views.connectftp, name='connectftp'),
+    re_path(r'byname$', views.FileWNameView.as_view(), name='byname'),
 ]
-
--- ftpapp/views.py
+++ ftpapp/views.py
@@ -1,9 +1,13 @@
-import binascii
-from django.http import HttpResponseRedirect, HttpResponse,JsonResponse
+import requests
+from ftpapp import auth, utils
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
 from ftplib import FTP
 import datetime
 import json
+import after_response
 
 with open('config.json', 'r') as f:
     config = json.load(f)
@@ -20,7 +24,7 @@ class BinaryData:
         self.buffer += data
 
 
-def downloadFile(ftp, filenames,charset):
+def download_by_date(ftp, filenames, charset):
     bd = BinaryData()
     for file in filenames:
         retr = ftp.retrbinary('RETR ' + file, bd.save_data_to_buff, 1024)
@@ -29,22 +33,21 @@ def downloadFile(ftp, filenames,charset):
     ftp.quit()
     return bd.buffer.decode('cp1251').encode('utf-8')
 
-def download_by_name(ftp,filename,charset):
+
+def download_by_name(ftp, filename):
     bd = BinaryData()
     retr = ftp.retrbinary('RETR ' + filename, bd.save_data_to_buff, 1024)
-    bd.save_data_to_buff(config['FILE_DELIMITER'].encode(charset))
-
     ftp.quit()
     return bd.buffer.decode('cp1251').encode('utf-8')
 
 
-def addFilename(arg):
+def add_filename(arg):
     splited_line = arg.split(' ')
     filename = splited_line[-1]
     filenames.append(filename)
 
 
-def createDateFileDict(ftp, req_date):
+def create_date_file_dict(ftp, req_date):
     request_date = datetime.datetime.strptime(req_date, config['DATE_FORMAT'])
     for filename in filenames:
         response = ftp.sendcmd('MDTM ' + filename)
@@ -54,36 +57,71 @@ def createDateFileDict(ftp, req_date):
         if file_date > request_date:
             dateToFileDict[file_date] = filename
 
+
 @csrf_exempt
 def connectftp(request):
     print(filenames)
     filenames.clear()
     dateToFileDict.clear()
     if request.method == 'POST':
-        date = request.POST.get('DATE','')
+        date = request.POST.get('DATE', '')
         if not date:
-            return JsonResponse({'status': 422 ,'message': 'DATE param is required'})
+            return JsonResponse({'status': 422, 'message': 'DATE param is required'})
         else:
             ftp = FTP()
             ftp.connect(request.POST.get('HOST', config['HOST']), int(request.POST.get('PORT', config['PORT'])))
             ftp.login(request.POST.get('LOGIN', config['LOGIN']), request.POST.get('PASSWORD', config['PASSWORD']))
             print('Connection!')
-            result = ftp.retrlines('LIST', addFilename)
+            result = ftp.retrlines('LIST', add_filename)
             if '226' in result:
-                createDateFileDict(ftp, date)
+                create_date_file_dict(ftp, date)
 
-            binary_data = downloadFile(ftp, dateToFileDict.values(), request.POST.get('CHARSET',config['CHARSET']))
+            binary_data = download_by_date(ftp, dateToFileDict.values(), request.POST.get('CHARSET', config['CHARSET']))
             return HttpResponse(binary_data, content_type='application/x-binary')
 
-@csrf_exempt
-def filebyname(request):
 
-    if request.method == 'POST':
-        filename = request.POST.get('FILENAME','')
-        ftp = FTP()
-        ftp.connect(request.POST.get('HOST', config['HOST']))
-        ftp.login(request.POST.get('LOGIN', config['LOGIN']), request.POST.get('PASSWORD', config['PASSWORD']))
-        print('Connection!')
-        binary_data = download_by_name(ftp, filename,request.POST.get('CHARSET',config['CHARSET']))
-        print('Data has downloaded')
-        return HttpResponse('200', content_type='application/x-binary')
+@method_decorator(csrf_exempt, name='dispatch')
+class FileByNameView(View):
+    pass
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class FileWNameView(View):
+
+    def post(self, request, *args, **kwargs):
+        ftp_con = utils.connect_ftp(request)
+        if ftp_con is None:
+            return HttpResponse('Can`t connect to FTP')
+        else:
+            filename = request.POST['FILENAME']
+            if ftp_con.retrlines('NLST', utils.check_file_curried(filename)):
+                process_after_response.after_response(ftp_con, filename)
+                ftp_con.quit()
+                return HttpResponse('Success')
+            else:
+                return HttpResponse(f'File {filename} doesn`t exist')
+
+
+@after_response.enable
+def process_after_response(ftp_con, filename):
+    print('After Response')
+    sf_url = 'https://epd-vision--turkeyimp.cs89.my.salesforce.com/services/apexrest/stopharm/price/'
+    try:
+        binary_data = download_by_name(ftp_con, filename)
+    finally:
+        ftp_con.quit()
+
+    token = auth.get_token_to_sf(**auth.credentials)
+    print(token)
+    if token is not None:
+        string_data = binary_data.decode('utf-8').replace('\r\n', '')
+        headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
+        data = {'data': string_data, 'city_code': filename.split('.')[0]}
+        response = requests.post(url=sf_url, data=json.dumps(data), headers=headers)
+        print(f'Response status : {response.status_code}')
+    else:
+        print('Invalid token')
+
+
+
+
