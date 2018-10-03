c80472c11236d7c36cf83a993eb40d1866a692ce
Igor <igor.ievsieienko@ctdev.io>
2018-10-03 16:26:28 +0300

    removed encode-decode

--- redirectapp/views.py
+++ redirectapp/views.py
@@ -31,4 +31,5 @@ def bills(request):
         response = requests.post(
             request.POST['url'],data={'BODY':request.POST['body']}, verify=False,
             stream=True)
-        return HttpResponse(response.raw.read().decode('cp1251').encode('utf-8'))
+        return HttpResponse(response.raw.read())
+            #.decode('cp1251').encode('utf-8'))
